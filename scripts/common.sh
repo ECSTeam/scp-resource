3cf887ed45d0e54e1773e1b9ece030389960a5bd
Josh Ghiloni <jghiloni@ecsteam.com>
2017-03-08 21:40:51 -0600

    Revert logging

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,6 +1,5 @@
-
-function output() {
-	echo "$@" >&3
+function log() {
+	echo "$@" 1>&2
 }
 
 function initialize() {
@@ -19,34 +18,34 @@ function initialize() {
 	# give a default to basedir
 	[[ ${basedir} == "null" ]] && basedir="."
 
-	local version=$(output ${input_json} | jq -r '.version')
-	[[ ${version} != "null" ]] && version=$(output "${version}" | jq -r '.path')
+	local version=$(echo ${input_json} | jq -r '.version')
+	[[ ${version} != "null" ]] && version=$(echo "${version}" | jq -r '.path')
 
-	echo "Initializing environment"
+	log "Initializing environment"
 
-	output "host='${host}'"
-	output "user='${user}'"
-	output "glob='${glob}'"
-	output "version='${version}'"
-	output "basedir='${basedir}'"
-	output "pkey='${pkey}'"
+	echo "host='${host}'"
+	echo "user='${user}'"
+	echo "glob='${glob}'"
+	echo "version='${version}'"
+	echo "basedir='${basedir}'"
+	echo "pkey='${pkey}'"
 }
 
 function init_ssh_auth() {
 	local pkey=$1
 	keyfile=$(mktemp)
 
-	pkey=$(output "${pkey}" | sed -e 's/-----BEGIN RSA PRIVATE KEY----- \(.*\) -----END RSA PRIVATE KEY-----/\1/' | tr ' ' '\n')
-	output "-----BEGIN RSA PRIVATE KEY-----" > ${keyfile}
-	output "${pkey}" >> ${keyfile}
-	output "-----END RSA PRIVATE KEY-----" >> ${keyfile}
+	pkey=$(echo "${pkey}" | sed -e 's/-----BEGIN RSA PRIVATE KEY----- \(.*\) -----END RSA PRIVATE KEY-----/\1/' | tr ' ' '\n')
+	echo "-----BEGIN RSA PRIVATE KEY-----" > ${keyfile}
+	echo "${pkey}" >> ${keyfile}
+	echo "-----END RSA PRIVATE KEY-----" >> ${keyfile}
 	chmod 600 ${keyfile}
 
 	ssh-keygen -l -f ${keyfile} > /dev/null 2>&1
 	is_good=$?
 
 	if [[ ${is_good} != 0 ]]; then
-		echo "Private key file is corrupt!"
+		log "Private key file is corrupt!"
 		exit ${is_good}
 	fi
 
@@ -62,29 +61,26 @@ function get_latest_files() {
 	local basedir=$5
 
 	files=$(ssh -o StrictHostKeyChecking=no ${user}@${host} "ls -lrt ${basedir}/${glob}")
-	files=$(output "${files}" | awk '/^\-/{print $(NF)}')
+	files=$(echo "${files}" | awk '/^\-/{print $(NF)}')
 
 	if [[ $version != null ]]; then
 		version=$(basename ${version})
 		grepcheck="^${basedir}/${version}$"
 
-		numfiles=$(output "${files}" | wc -l | xargs)
-		files=$(output "${files}" | grep -A ${numfiles} "${grepcheck}")
+		numfiles=$(echo "${files}" | wc -l | xargs)
+		files=$(echo "${files}" | grep -A ${numfiles} "${grepcheck}")
 	else
-		files=$(output "${files}" | head -n 1)
+		files=$(echo "${files}" | head -n 1)
 	fi
 
-	output "${files}"
+	echo "${files}"
 }
-
-exec 3>&1 # make stdout available as fd 3 for the result
-exec 1>&2 # redirect all output to stderr for logging
 
 input_json=$(</dev/stdin)
 
 vars=$(initialize "${input_json}")
 
-output ${vars} > /tmp/vars
+echo ${vars} > /tmp/vars
 eval ${vars}
 
 init_ssh_auth "${pkey}"
